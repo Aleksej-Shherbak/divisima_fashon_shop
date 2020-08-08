fd6f13afc24db8e2ba595d677c1a30762eefdf7e
aleksej <aleksej.shherbak@yandex.ru>
2020-08-08 16:39:46 +0300

    Added utils

---
+++ Utils/ObjectUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Utils
+{
+    public static class ObjectUtils
+    {
+        public static byte[] ConvertAnyObjectToByteArray(object obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+
+            BinaryFormatter bf = new BinaryFormatter();
+            using MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, obj);
+            return ms.ToArray();
+        }
+
+        public static T ConvertByteArrayToObject<T>(byte[] arr)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using MemoryStream ms = new MemoryStream(arr);
+            object obj = bf.Deserialize(ms);
+            return (T) obj;
+        }
+    }
+}
